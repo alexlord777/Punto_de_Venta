86ea2059c76235665006e750ab919335ba511734
unknown <1803684g@umich.mx>
2023-07-11 12:57:43 -0500

    controllers

--- Api/src/v1/routes/workoutRoutes.js
+++ Api/src/v1/routes/workoutRoutes.js
@@ -1,4 +1,15 @@
 const express= require("express");
 const router= express.Router();
 
+//const workoutCotrollers= require
+
+
+router
+     .get("/",)
+     .get("/:productID",)
+     .post("/",)
+     .patch("/:productID")
+     .delete("/:productID")
+
+     
 module.exports=router;
