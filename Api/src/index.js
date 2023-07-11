7e7ba660b443ff810bfa0632458be390684e4258
unknown <1803684g@umich.mx>
2023-07-10 21:55:18 -0500

    routes creates

---
+++ Api/src/index.js
@@ -0,0 +1,12 @@
+const expres= require("express");
+const  v1WorkoutRouter= require("./v1/routes/workoutRoutes")
+
+const app= expres();
+const PORT= 3000;
+
+app.use(expres.json())
+app.use( '/hola' ,v1WorkoutRouter)
+
+app.listen(PORT,()=>{
+    console.log(`Server listen on port ${PORT}`);
+});
